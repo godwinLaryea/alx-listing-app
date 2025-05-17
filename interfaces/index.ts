7099b84aeb81e6a379e2408b73cf43abe6b9849d
godwinLaryea <kalexgun@gmail.com>
2025-05-17 14:10:45 +0000

    button and card props

---
+++ interfaces/index.ts
@@ -0,0 +1,10 @@
+export interface CardProps{
+  name: "",
+  price: "",
+  location: "";
+};
+
+export interface ButtonProps{
+  variant: "primary" | "secondary" | "outline",
+  isLoading: boolean;
+}
