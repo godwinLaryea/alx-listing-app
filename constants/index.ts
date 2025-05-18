952189cf4fdf853c0609423b13c0b30b87fe7bf8
godwinLaryea <kalexgun@gmail.com>
2025-05-18 18:17:54 +0000

    added reusable data

---
+++ constants/index.ts
@@ -0,0 +1,9 @@
+export const API = "https://api.example.com";
+
+
+//Status Messages
+export const STATUS_MESSAGES = {
+  loading: "Loading...",
+  success: "Operation successful!",
+  error: "Something went wrong. Please try again.",
+};
